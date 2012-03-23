969d0f9f086a1645d729df23a346ed9bc5d6544b
bandung <bandung@forever.(none)>
2012-03-23 23:16:29 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'b4909a588c376afd0b330f7f32a2544ae2c22fa7b1b7042ed07a84a4d6c325bf81d54bee01192ff120de0554985feea8674a810710bd8709a68f3b27a39a38a0'
